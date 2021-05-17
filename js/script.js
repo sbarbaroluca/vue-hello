b5a060e586f64e12475ecea65ceddfb06afe6cf8
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-17 16:18:24 +0200

    hello world

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 // Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
 // Aggiungere alla pagina un'immagine, presa anch'essa da un data.
 
+var app = new Vue(
+  {
+    el: "#root",
+    data: {
+      message: 'Hello World !',
+      classColor: 'red',
+    },
+    methods: {
+      colorChange: function() {
 
+        if (this.classColor = "red") {
+          this.classColor = "blue";
+        }
+        else if (this.classColor = "blue") {
+          this.classColor = "yellow";
+        } else {
+          this.classColor = "red";
+        }
+      }
+    }
+  }
+);
